afc7827ceb76e0a7b965b43d4d46f36062b87b92
cwanylisek <rewiks@gmail.com>
2020-01-21 15:31:40 +0100

    animation && arrows

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -11,13 +11,18 @@ const Slider = () => {
         <Slide key={photo.id} image={photo.photo} text={photo.text} category={photo.category} />
     ))
 
+    const slideAnimation = {
+        transform: `translateX(${translateValue}px)`,
+        transition: 'transform ease-in-out 0.2s'
+    }
+
     return (
-        <div className="slider">
-            <div className="slider__container">
+        <div className="slider"> 
+            <div className="slider__container" style={slideAnimation}>
                 {slide}
             </div>
-            <button onClick={leftArrow}>strzałka w lewo</button>
-            <button onClick={rightArrow}>strzałka w prawo</button>  
+            <button className="slider__left-arrow" onClick={leftArrow}>strzałka w lewo</button>
+            <button className="slider__right-arrow" onClick={rightArrow}>strzałka w prawo</button> 
         </div>
     )
 
