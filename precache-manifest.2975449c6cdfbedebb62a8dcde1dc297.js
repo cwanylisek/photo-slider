30797d88bbc78d6304482924e6898f861f03b143
root <rewiks@gmail.com>
2020-01-23 00:05:59 +0100

    Updates

---
+++ precache-manifest.2975449c6cdfbedebb62a8dcde1dc297.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1132711ad00b5f6518c156fb8c40ec88",
+    "url": "/photo-slider/index.html"
+  },
+  {
+    "revision": "07df90e36380335d112b",
+    "url": "/photo-slider/static/css/main.797f5d45.chunk.css"
+  },
+  {
+    "revision": "35849124dff4e1b97821",
+    "url": "/photo-slider/static/js/2.53b32ed6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/photo-slider/static/js/2.53b32ed6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07df90e36380335d112b",
+    "url": "/photo-slider/static/js/main.9e5c2840.chunk.js"
+  },
+  {
+    "revision": "4f95e1eb517987a53665",
+    "url": "/photo-slider/static/js/runtime-main.317c275d.js"
+  },
+  {
+    "revision": "3df142d56b9082d1d5f3c39008ac2983",
+    "url": "/photo-slider/static/media/kircholm.3df142d5.jpg"
+  },
+  {
+    "revision": "462ac6ad2e21072c2fb79ab295e19a59",
+    "url": "/photo-slider/static/media/nature.462ac6ad.jpg"
+  },
+  {
+    "revision": "bcadb9e7a8a85067683603b41ed0d84a",
+    "url": "/photo-slider/static/media/ramen.bcadb9e7.jpg"
+  }
+]);
